c61b159b3d2448cb8ba1458e3b522e8579b29c15
nsaranraj <saranrajn@gmail.com>
2022-01-20 20:01:50 +0530

    contact us add S

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href=" {{url('home')}}">Home</a>
+    <a href=" {{url('contact')}}">contact</a>
+
+    <h1>This is my  <span style="color:red"> contact </span> page</h1>
+</body>
+</html>
